e64579dd1820aa9b7b84df182aed9a7cbd1b42b9
longs7 <longkeyvic1@gmail.com>
2021-09-15 08:30:15 +0700

    add service implement

--- src/main/java/com/longs7/spingbootapi/service/BookService.java
+++ src/main/java/com/longs7/spingbootapi/service/BookService.java
@@ -5,4 +5,10 @@ import com.longs7.spingbootapi.entity.Book;
 import java.util.List;
 
 public interface BookService {
+    List<Book> findAllBook();
+    Book getBookById(int id);
+    void addBook(Book book);
+    void updateBook(Book book);
+    void deleteBook(int id);
+
 }
--- src/main/java/com/longs7/spingbootapi/service/BookServiceImpl.java
+++ src/main/java/com/longs7/spingbootapi/service/BookServiceImpl.java
@@ -1,4 +1,37 @@
 package com.longs7.spingbootapi.service;
 
+import com.longs7.spingbootapi.entity.Book;
+import com.longs7.spingbootapi.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public List<Book> findAllBook() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Book getBookById(int id) {
+        return bookRepository.findById(id).get();
+    }
+
+    @Override
+    public void addBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Override
+    public void updateBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    @Override
+    public void deleteBook(int id) {
+        bookRepository.deleteById(id);
+    }
 }
